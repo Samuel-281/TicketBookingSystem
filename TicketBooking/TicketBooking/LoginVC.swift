5d8ce4f35fbde2b479c358eb62b24ce20a86da3f
Samuel Tan <530249138@qq.com>
2022-05-09 19:06:18 +0800

    Login and Sign page
    Storyboard design

---
+++ TicketBooking/TicketBooking/LoginVC.swift
@@ -0,0 +1,19 @@
+//
+//  LoginVC.swift
+//  TicketBooking
+//
+//  Created by Samuel Tan on 2022/5/9.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
