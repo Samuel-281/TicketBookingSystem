010fd1e0a860f13e435cdf34fa9c7aa424ebc37f
Samuel Tan <530249138@qq.com>
2022-05-17 14:44:18 +0800

    Revert "Login function"
    This reverts commit ed23a9e3e2b86a936f0a7b3796e77c28af133385.

--- TicketBooking/TicketBooking/LoginVC.swift
+++ TicketBooking/TicketBooking/LoginVC.swift
@@ -8,35 +8,10 @@
 import UIKit
 
 class LoginVC: UIViewController {
-    
-    @IBOutlet weak var Username: UITextField!
-    @IBOutlet weak var Password: UITextField!
-    @IBOutlet weak var ErrorLabel: UILabel!
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "success",
-           let destination = segue.destination as? SuccessVC{
-                destination.text = "Login successful"
-            }
-        
-    }
-    
-    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
-        
-        if identifier == "success" {
-            if Username.text == nil || Username.text == ""{
-                ErrorLabel.text = "Please enter Username"
-                return false;
-            }
-            
-            if Password.text == nil || Password.text == ""{
-                ErrorLabel.text = "Please enter Password"
-                return false;
-            }
-            
-            ErrorLabel.text = nil
-        }
-        return true
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
 
 
